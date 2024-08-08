faa146d516c4d1da8b80d6dace84c1afe5ea022f
maxim <trifonov.max@mail.ru>
2024-08-08 21:20:46 +0300

    base

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+
+import { Header } from './components';
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <Box>app</Box>
+    </>
+  );
+};
