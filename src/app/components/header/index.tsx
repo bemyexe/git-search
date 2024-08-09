7a7f066db3b99d437dd3c14cc54fc35d1748814f
maxim <trifonov.max@mail.ru>
2024-08-09 08:11:56 +0300

    feature(components): added header

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,5 +1,13 @@
 import { Box } from '@mui/material';
 
+import { SearchPanel } from '../shared/search-panel';
+
+import './style.scss';
+
 export const Header = () => {
-  return <Box component="header">Header</Box>;
+  return (
+    <Box component="header" className="header">
+      <SearchPanel />
+    </Box>
+  );
 };
