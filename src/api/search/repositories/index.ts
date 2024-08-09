d5c1a30770c6c27fb18b6d3b6261082995d79894
maxim <trifonov.max@mail.ru>
2024-08-09 22:22:17 +0300

    feature(api): added api requests

---
+++ src/api/search/repositories/index.ts
@@ -0,0 +1,20 @@
+import { Data } from '../../../../@types';
+import { instance } from '../../instance';
+
+const SEARCH_REPOSITOIRES_BASE_URL = 'search/repositories';
+
+export const getRepositories = async (query, page, per_page, sort, order) => {
+  const searchParams: Record<string, any> = new URLSearchParams({
+    page: page,
+    per_page: '' + per_page,
+    sort: sort,
+    order: order,
+  });
+  const response = await instance.get<Data>(
+    `${SEARCH_REPOSITOIRES_BASE_URL}?q=${query}`,
+    {
+      params: searchParams,
+    }
+  );
+  return response.data;
+};
