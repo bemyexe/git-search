38f55f9590037b0dcef9aa2ea755f0a03b707a37
maxim <trifonov.max@mail.ru>
2024-08-10 17:59:04 +0300

    feature(components): added repo-card

--- src/app/components/shared/repo-card/index.tsx
+++ src/app/components/shared/repo-card/index.tsx
@@ -1,7 +1,37 @@
-import { Box } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { Box, Chip, Typography } from '@mui/material';
+
+import { repoCardSelectors } from '../../../../store/repo-card/repo-card.selectors';
 
 import './style.scss';
 
 export const RepoCard = () => {
-  return <Box className="repo-card">Box</Box>;
+  const card = useSelector(repoCardSelectors.selectRepoCard);
+  const isRepoChosen = useSelector(repoCardSelectors.selectisRepoChosen);
+  return (
+    <Box className="repo-card">
+      {isRepoChosen ? (
+        <Box className="repo-card-container">
+          <Typography variant="h4">Название репозитория</Typography>
+          <Chip label={card?.name} color="primary" />
+          <Chip
+            label={card?.description}
+            sx={{
+              height: 'auto',
+              '& .MuiChip-label': {
+                display: 'block',
+                whiteSpace: 'normal',
+                padding: '10px',
+              },
+            }}
+          />
+          <Typography variant="caption">
+            {card?.license?.name || 'No license'}
+          </Typography>
+        </Box>
+      ) : (
+        <Typography className="no-repo">Выберите репоризотрий</Typography>
+      )}
+    </Box>
+  );
 };
