a059d6ed155dbfa12827a575538dbda077ea4ea5
maxim <trifonov.max@mail.ru>
2024-08-10 18:08:07 +0300

    refacor(repo-card): refactor

--- src/app/components/shared/repo-card/index.tsx
+++ src/app/components/shared/repo-card/index.tsx
@@ -14,20 +14,22 @@ export const RepoCard = () => {
         <Box className="repo-card-container">
           <Typography variant="h4">Название репозитория</Typography>
           <Chip label={card?.name} color="primary" />
-          <Chip
-            label={card?.description}
-            sx={{
-              height: 'auto',
-              '& .MuiChip-label': {
-                display: 'block',
-                whiteSpace: 'normal',
-                padding: '10px',
-              },
-            }}
-          />
-          <Typography variant="caption">
-            {card?.license?.name || 'No license'}
-          </Typography>
+          {!!card?.description && (
+            <Chip
+              label={card?.description}
+              sx={{
+                height: 'auto',
+                '& .MuiChip-label': {
+                  display: 'block',
+                  whiteSpace: 'normal',
+                  padding: '10px',
+                },
+              }}
+            />
+          )}
+          {!!card?.license?.name && (
+            <Typography variant="caption">{card?.license?.name}</Typography>
+          )}
         </Box>
       ) : (
         <Typography className="no-repo">Выберите репоризотрий</Typography>
