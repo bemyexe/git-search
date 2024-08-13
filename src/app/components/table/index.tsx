64ae1d0ede610da4eee29bae6afed91f2fdb9bf2
maxim <trifonov.max@mail.ru>
2024-08-13 15:18:47 +0300

    refactor(table): enhanced logic

--- src/app/components/table/index.tsx
+++ src/app/components/table/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { useSelector } from 'react-redux';
 import { Paper } from '@mui/material';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
@@ -11,63 +10,62 @@ import TableRow from '@mui/material/TableRow';
 
 import { Repository } from '../../../../@types';
 import { formatDate } from '../../../helpers/date-formatter';
-import { getComparator } from '../../../helpers/get-comparator';
-import { stableSort } from '../../../helpers/stable-sort';
 import { useAppDispatch } from '../../../store';
-import { filterSelectors } from '../../../store/filter/filter.selectors';
 import {
   setOrderValue,
   setPageValue,
   setPerPageValue,
+  setSortValue,
 } from '../../../store/filter/filter.slice';
 import { setRepoCardValue } from '../../../store/repo-card/repo-card.slice';
-import { reposSelectors } from '../../../store/repos/repos.selectors';
 
 import { EnhancedTableHead } from './enhanced-table-head';
 
 import './style.scss';
 
-export const EnhancedTable = () => {
+interface EnhancedTableProps {
+  sort: Sort;
+  order: Order;
+  repos: Repository[];
+  reposTotalCount: number;
+  page: number;
+  rowsPerPage: number;
+  isLoading: boolean;
+}
+
+const ROWS_PER_PAGE_OPTIONS = [5, 10, 25];
+const PARSE_INT_RADIX = 10;
+
+export const EnhancedTable = ({
+  sort,
+  order,
+  repos,
+  reposTotalCount,
+  page,
+  rowsPerPage,
+  isLoading,
+}: EnhancedTableProps) => {
   const dispatch = useAppDispatch();
 
-  // const [order, setOrder] = React.useState<Order>('asc');
-  const [orderBy, setOrderBy] = React.useState<keyof Repository>('name');
-  // const [rowsPerPage, setRowsPerPage] = React.useState(5);
-  const search = useSelector(reposSelectors.selectReposList);
-  const order = useSelector(filterSelectors.selectOrderValue);
-
-  const page = useSelector(filterSelectors.selectPageValue);
-  const rowsPerPage = useSelector(filterSelectors.selectPerPageValue);
-
   const handleRequestSort = (
-    event: React.MouseEvent<unknown>,
-    property: keyof Repository
+    _event: React.MouseEvent<unknown>,
+    property: Sort
   ) => {
-    const isAsc = orderBy === property && order === 'asc';
+    const isAsc = sort === property && order === 'asc';
     dispatch(setOrderValue(isAsc ? 'desc' : 'asc'));
-    setOrderBy(property);
+    dispatch(setSortValue(property));
   };
 
-  const handleChangePage = (event: unknown, newPage: number) => {
-    dispatch(setPageValue(newPage));
+  const handleChangePage = (_event: unknown, newPage: number) => {
+    dispatch(setPageValue(newPage + 1));
   };
 
   const handleChangeRowsPerPage = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    dispatch(setPerPageValue(parseInt(event.target.value, 10)));
-    dispatch(setPageValue(0));
+    dispatch(setPerPageValue(parseInt(event.target.value, PARSE_INT_RADIX)));
+    dispatch(setPageValue(1));
   };
-  // Avoid a layout jump when reaching the last page with empty rows.
-
-  const visibleRows = React.useMemo(
-    () =>
-      search &&
-      stableSort(search as Repository[], getComparator(order, orderBy)),
-    [order, orderBy, page, rowsPerPage, search]
-  );
-  console.log('aa00', visibleRows);
-  const load = useSelector(reposSelectors.selectReposLoading);
 
   return (
     <Box className="table-wrapper">
@@ -76,46 +74,48 @@ export const EnhancedTable = () => {
           <Table aria-labelledby="tableTitle">
             <EnhancedTableHead
               order={order}
-              orderBy={orderBy}
+              orderBy={sort}
               onRequestSort={handleRequestSort}
             />
             <TableBody>
-              {load
-                ? 'load'
-                : visibleRows?.map((row, index) => {
-                    const labelId = `enhanced-table-checkbox-${index}`;
+              {isLoading ? (
+                <TableRow>
+                  <TableCell>Loading...</TableCell>
+                </TableRow>
+              ) : (
+                repos?.map((row, index) => {
+                  const labelId = `enhanced-table-checkbox-${index}`;
 
-                    return (
-                      <TableRow
-                        className="table-body-row"
-                        hover
-                        onClick={() => dispatch(setRepoCardValue(row))}
-                        tabIndex={-1}
-                        key={row.id}
-                      >
-                        <TableCell component="th" id={labelId} scope="row">
-                          {row.name}
-                        </TableCell>
-                        <TableCell align="left">{row.language}</TableCell>
-                        <TableCell align="left">{row.forks_count}</TableCell>
-                        <TableCell align="left">
-                          {row.stargazers_count}
-                        </TableCell>
-                        <TableCell align="left">
-                          {formatDate(row.updated_at)}
-                        </TableCell>
-                      </TableRow>
-                    );
-                  })}
+                  return (
+                    <TableRow
+                      className="table-body-row"
+                      hover
+                      onClick={() => dispatch(setRepoCardValue(row))}
+                      tabIndex={-1}
+                      key={row.id}
+                    >
+                      <TableCell component="th" id={labelId} scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="left">{row.language}</TableCell>
+                      <TableCell align="left">{row.forks_count}</TableCell>
+                      <TableCell align="left">{row.stargazers_count}</TableCell>
+                      <TableCell align="left">
+                        {formatDate(row.updated_at)}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })
+              )}
             </TableBody>
           </Table>
         </TableContainer>
         <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
+          rowsPerPageOptions={ROWS_PER_PAGE_OPTIONS}
           component="div"
-          count={-1}
+          count={reposTotalCount ?? 0}
           rowsPerPage={rowsPerPage}
-          page={page}
+          page={page - 1}
           onPageChange={handleChangePage}
           onRowsPerPageChange={handleChangeRowsPerPage}
         />
